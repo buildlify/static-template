4a8a60af71db56e36f91b4010a4453b202dea130
Austin Klein <austinklein34@gmail.com>
2022-09-27 10:34:38 -0600

    finishing template

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.ts?(x)': () => 'yarn build-types',
-  '*.json': ['prettier --write'],
-};
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Popover, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline/index';
 import { Fragment } from 'react';
 
 const links = [
